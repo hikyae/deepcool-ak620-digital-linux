4ef82c5fb0f86e1311e8570e5ab87a8a200c75b9
hikyae <h1ky43@gmail.com>
2025-01-17 13:48:49 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo cp -rf deepcool-ak620-digital.service /usr/lib/systemd/system/
+sudo cp -rf deepcool-ak620-digital-restart.service /usr/lib/systemd/system/
+sudo cp -rf deepcool-ak620-digital /usr/bin/deepcool-ak620-digital
+
